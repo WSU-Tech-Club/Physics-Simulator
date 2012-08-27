2728b9d5837eef7c701be0f35950134392b565e8
Chris Corboy <christopher.corboy@email.wsu.edu>
2012-08-27 13:17:57 -0700

    basic skeleton for the base class

---
+++ Physics-Sim/BaseObject.h
@@ -0,0 +1,29 @@
+//Class name: BaseObject
+//Author:Chris Corboy
+//Created: 8/27/12
+//Last Revision:8/27/12
+//Summary: very basic skeleton for the base class
+//
+
+#ifndef BASEOBJECT_H
+#define BASEOBJECT_H
+
+#include "Vector.h"
+
+namespace PhysicsSim
+{
+
+class BaseObject
+{
+public:
+
+	PSVect *position;
+	PSVect *velocity;
+	PSVect *accel;
+
+	BaseObject ();
+};
+
+
+}
+#endif
